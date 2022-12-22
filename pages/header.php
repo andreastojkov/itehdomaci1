fb0c28612c98937ff96905e21d62306f6370e930
Andrea Stojkov <andreastojkov00@gmail.com>
2022-12-22 18:36:29 +0100

    Napravljen header, index, stranice za termine

---
+++ pages/header.php
@@ -0,0 +1,18 @@
+<div class='navbar_div'>  
+  <nav class="navbar navbar-light sticky-top bottom_sep" style="background-color: #B7ADCF;">
+    <img src="../img/health.png" class="logo-pic"/> 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Usluge</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="termini.php">Termini</a>
+        </li>
+        </ul>
+    </div>
+  </nav>
+</div>
