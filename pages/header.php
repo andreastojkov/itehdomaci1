acfe56876f44c4d1e5bde4d9c277564d13d48824
Andrea Stojkov <andreastojkov00@gmail.com>
2022-12-28 19:25:53 +0100

    Izmenjen header finalno

--- pages/header.php
+++ pages/header.php
@@ -1,26 +1,9 @@
-<!-- <div class='navbar_div'>  
-  <nav class="navbar navbar-light sticky-top bottom_sep" style="background-color: #B7ADCF;">
-    <img src="../img/health.png" class="logo-pic"/> 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="index.php">Usluge</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="termini.php">Termini</a>
-        </li>
-        </ul>
-    </div>
-  </nav>
-</div> -->
+
 <div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbardec">
   <a class="navbar-brand" href="#">DOM ZDRAVLJA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
