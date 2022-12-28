ffd8dbcca5d120883d542838c407a56f96b7a809
Andrea Stojkov <andreastojkov00@gmail.com>
2022-12-28 14:00:00 +0100

    Promenjen header i sadržaj

--- pages/header.php
+++ pages/header.php
@@ -1,4 +1,4 @@
-<div class='navbar_div'>  
+<!-- <div class='navbar_div'>  
   <nav class="navbar navbar-light sticky-top bottom_sep" style="background-color: #B7ADCF;">
     <img src="../img/health.png" class="logo-pic"/> 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,4 +15,55 @@
         </ul>
     </div>
   </nav>
+</div> -->
+<div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbardec">
+  <a class="navbar-brand" href="#">DOM ZDRAVLJA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php"> USLUGE </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="termini.php"> TERMINI </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          LOKACIJE
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="http://www.dzvozdovac.rs/">Voždovac</a>
+          <a class="dropdown-item" href="https://dzvracar.org.rs/">Vračar</a>
+          <a class="dropdown-item" href="https://dzzvezdara.rs/">Zvezdara</a>
+          <a class="dropdown-item" href="http://www.dzzemun.org.rs/">Zemun</a>
+          <a class="dropdown-item" href="http://www.dznbgd.com/">Novi Beograd</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="https://www.beograd.rs/lat/zivot-u-beogradu/1784-domovi-zdravlja/">Spisak svih domova zdravlja</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"> MAPA - USKORO </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
 </div>
